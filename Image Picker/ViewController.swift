7f4a2fb3d016070d93dd98f7cfca93ead5d82cc4
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-16 12:02:06 -0300

    Create a basic image picker swapping through the different options

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    var i = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated.g
     }
 
-
+    @IBAction func selectImage(_ sender: UIButton) {
+        self.imagePicker.allowsEditing = false
+        switch self.i {
+        case 0:
+            self.imagePicker.sourceType = .photoLibrary
+        case 1:
+            self.imagePicker.sourceType = .savedPhotosAlbum
+        default:
+            // self.imagePicker.sourceType = .camera
+            print("crashs")
+        }
+        self.i = self.i == 2 ? 0 : self.i + 1
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
