7af95ecaf087d10a861657f95f9fa2f1d16e3a87
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-16 11:16:19 -0300

    Initial Commit

---
+++ Image Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Image Picker
+//
+//  Created by Giovanni Pires da Silva on 16/07/2018.
+//  Copyright © 2018 Ezoom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Image PickerTests/Image_PickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Image_PickerTests.swift
+//  Image PickerTests
+//
+//  Created by Giovanni Pires da Silva on 16/07/2018.
+//  Copyright © 2018 Ezoom. All rights reserved.
+//
+
+import XCTest
+@testable import Image_Picker
+
+class Image_PickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
